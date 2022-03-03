9a4fd2aab11744a9943082b3bc030bb2d0ab8242
Milan Lee <yooossi@gmail.com>
2022-03-03 21:42:49 +0900

    script 추가 >> 현정님께서 정해주신  listcmt(), postcmt()로 추가했습니다!

--- app.py
+++ app.py
@@ -7,18 +7,26 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index_sub.html')
 
 @app.route("/hiking", methods=["POST"])
 def hiking_post():
+    name_receive = request.form["name_give"]
     star_receive = request.form["star_give"]
     comment_receive = request.form["comment_give"]
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+        'star': star_receive
+    }
+    db.hiking.insert_one(doc)
     return jsonify({'msg':'등록 완료'})
 
 @app.route("/hiking", methods=["GET"])
 def hiking_get():
-    review_list = list(db.hiking.find({}, {'_id': False}))
-    return jsonify({'reviews':review_list})
+    cmt_list = list(db.hiking.find({}, {'_id': False}))
+    return jsonify({'comment':cmt_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
