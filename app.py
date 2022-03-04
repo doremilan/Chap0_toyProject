86d7b84194eaa9d9b2a56f68800795044e9e9474
jinju-woo <100115394+jinju-woo@users.noreply.github.com>
2022-03-04 16:39:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,27 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
-@app.route("/hiking", methods=["POST"])
-def hiking_post():
-    star_receive = request.form["star_give"]
-    comment_receive = request.form["comment_give"]
-    return jsonify({'msg':'등록 완료'})
+@app.route('/index_sub')
+def index_sub():
+    return render_template('index_sub.html')
 
-@app.route("/hiking", methods=["GET"])
-def hiking_get():
-    review_list = list(db.hiking.find({}, {'_id': False}))
-    return jsonify({'reviews':review_list})
+@app.route("/mnt_info", methods=["POST"])
+def movie_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg':'POST 연결 완료!'})
+
+#  지역별 필터 기능
+@app.route("/mnt_select", methods=["POST"])
+def mnt_select():
+    area_receive = request.form["area_give"]
+    search_list = list(db.mnt_info.find({"mnt_area": area_receive}, {'_id': False}))
+    return jsonify({'area_list':search_list})
+
+@app.route("/mnt_info", methods=["GET"])
+def mnt_get():
+    all_mnt = list(db.mnt_info.find({},{'_id':False}))
+    return jsonify({'mnt': all_mnt})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
